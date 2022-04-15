c2f7a1f49bde51e85e67e3228bc9dfe8ef526418
Sundar0403 <97290440+Sundar0403@users.noreply.github.com>
2022-04-15 18:40:10 +0530

    Add files via upload

--- matrixMain/MatrixMain.java
+++ matrixMain/MatrixMain.java
@@ -22,6 +22,41 @@ public class MatrixMain
 		}
 		logicObj.replaceZeros(arr);
 	}
+	public void diagonalSorting()
+	{
+		int num=scan.nextInt();
+		scan.nextLine();
+		int arr[][]=new int[num][num];
+		for(int i=0;i<num;i++)
+		{
+			for(int j=0;j<num;j++)
+			{
+				arr[i][j]=scan.nextInt();
+				//scan.nextLine();
+			}
+		}
+		logicObj.diagonalSorting(arr);
+	}
+	
+	public void letsPlay()
+	{
+		int num=scan.nextInt();
+		scan.nextLine();
+		int col=scan.nextInt();
+		scan.nextLine();
+		int x=scan.nextInt();
+		scan.nextLine();
+		int arr[][]=new int[num][num];
+		for(int i=0;i<num;i++)
+		{
+			for(int j=0;j<col;j++)
+			{
+				arr[i][j]=scan.nextInt();
+				//scan.nextLine();
+			}
+		}
+		System.out.println(logicObj.isSuperSimilar(num,col,arr,x));
+	}
 	
 	public static void main(String args[])
 	{
@@ -36,6 +71,16 @@ public class MatrixMain
 			case 5:
 				mainObj.replaceZeros();
 				break;
+				
+			case 7:
+				mainObj.diagonalSorting();
+				break;
+				
+			case 10:
+				mainObj.letsPlay();
+				break;	
+				
+					
 		}
 	}
 }
